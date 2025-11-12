eae2107da56291e84c8e3c9154c3f50c8dfaddec
Patricia Martínez Espert <patrimespert@outlook.com>
2025-11-12 07:53:11 +0100

    Initial Commit

---
+++ Scores/ScoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoresApp.swift
+//  Scores
+//
+//  Created by Patricia M Espert on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
