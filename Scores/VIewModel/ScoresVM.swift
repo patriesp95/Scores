db74fdf9360d79fef4a3188888c3cd45e63a39ed
Patricia Martínez Espert <patrimespert@outlook.com>
2025-11-13 07:03:07 +0100

    viewmodel layer

---
+++ Scores/VIewModel/ScoresVM.swift
@@ -0,0 +1,24 @@
+//
+//  ScoresVM.swift
+//  Scores
+//
+//  Created by Patricia M Espert on 13/11/25.
+//
+
+import SwiftUI
+import Combine
+
+final class ScoresVM: ObservableObject {
+    @Published var scores: [Score]
+    let repository: DataRepository
+    
+    init(repository: DataRepository = Repository()) {
+        self.repository = repository
+        do {
+            self.scores = try repository.load()
+        } catch {
+            self.scores = []
+            print(error)
+        }
+    }
+}
